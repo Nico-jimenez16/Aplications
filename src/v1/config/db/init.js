1a4e8327109b98854c152570f4c3b8860dd16a39
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2024-01-02 21:00:20 -0300

    gitignore and add function disconnect to database

--- src/v1/config/db/init.js
+++ src/v1/config/db/init.js
@@ -6,3 +6,7 @@ const db = new Database(envs.URIMONGODB);
 export default async function ConnectToDatabase() {
   await db.connect();
 }
+
+export default async function DisconnectToDatabase() {
+  await db.disconnect()
+}
